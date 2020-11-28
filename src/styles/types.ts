ae77057699eceb8ccd3a5a4b9e2262158dd41b99
J M Rossy <jm.rossy@gmail.com>
2020-11-28 11:35:16 -0500

    Improve TS compile + intellisense times by disabling css type checking for now

--- src/styles/types.ts
+++ src/styles/types.ts
@@ -1,5 +1,10 @@
-import { CSSPropertiesWithMultiValues, CSSPseudos } from '@emotion/serialize'
-
-export type Styles = CSSPropertiesWithMultiValues & CSSPseudos
+/**
+ * This used to be a real type like CSSPropertiesWithMultiValues & CSSPseudos
+ * But that after some tsc compilation debugging, I found that was causing
+ * the compilation times to grow 2-3x, only worsening as the project grows.
+ * Made development in VSCode pretty painful. So I'm sacrificing css type safety
+ * in favor of fast intellisense for now :(
+ */
+export type Styles = any
 
 export type Stylesheet = Record<string, Styles>
