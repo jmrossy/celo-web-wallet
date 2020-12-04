58990b25aa2b87764659e3e2326fb1bf4abe548a
Brian Schwalm <bschwalm@anythinksolutions.com>
2020-12-04 15:11:46 -0700

    removing some unnecessary comments.

--- src/index.tsx
+++ src/index.tsx
@@ -8,14 +8,7 @@ import 'src/styles/fonts.css'
 import 'src/styles/normalize.css'
 import 'src/styles/scrollbar.css'
 
-//The following is to display the loader for a minimum of 2 seconds, even on fast connections
-// const loader = document.querySelector('.loader')
-// const hideLoader = () => loader?.classList.add('loader--hide')
-// const startStr = loader?.getAttribute('data-start')
-// const startTime = startStr ? parseInt(startStr) : Date.now()
-
 const mountNode = document.getElementById('app')
-
 ReactDOM.render(
   <Provider store={store}>
     <PersistGate loading={<Loading />} persistor={persistor}>
