8097777a609815acc804398c2e1ffb37e0cd2b82
J M Rossy <jm.rossy@gmail.com>
2020-12-09 15:13:52 -0500

    Fix spacing around ViewWalletScreen

--- src/features/wallet/ViewWalletScreen.tsx
+++ src/features/wallet/ViewWalletScreen.tsx
@@ -2,9 +2,11 @@ import { useNavigate } from 'react-router'
 import { Button } from 'src/components/Button'
 import ArrowBackIcon from 'src/components/icons/arrow_back.svg'
 import { Box } from 'src/components/layout/Box'
+import { ScreenContentFrame } from 'src/components/layout/ScreenContentFrame'
 import { WalletDetails } from 'src/features/wallet/WalletDetails'
 import { Color } from 'src/styles/Color'
 import { Font } from 'src/styles/fonts'
+import { mq } from 'src/styles/mediaQueries'
 import { Stylesheet } from 'src/styles/types'
 
 export function ViewWalletScreen() {
@@ -16,31 +18,38 @@ export function ViewWalletScreen() {
 
   // TODO the routes and HomeFrame need to be refactored a bit
   // to have this render without the feed on the left
-  //TODO: consider wrapping in the ScreenContentFrame for consistent spacing.
-  // Not sure if there's anything that needs to be different here (minHeight 100%?)
   return (
-    <Box
-      direction="column"
-      align="start"
-      justify="start"
-      styles={style.contentContainer}
-      margin="1.2em 1.4em"
-    >
-      <h1 css={style.header}>Your Celo Account</h1>
-      <WalletDetails />
-      <Button color={Color.altGrey} icon={ArrowBackIcon} onClick={onClickBack} margin="3em 0 0 0">
-        Back
-      </Button>
-    </Box>
+    <ScreenContentFrame>
+      <Box direction="column" align="center" styles={style.contentContainer}>
+        <h1 css={style.header}>Your Celo Account</h1>
+        <div css={style.detailsContainer}>
+          <WalletDetails />
+        </div>
+        <Button color={Color.altGrey} icon={ArrowBackIcon} onClick={onClickBack} margin="3em 0">
+          Back
+        </Button>
+      </Box>
+    </ScreenContentFrame>
   )
 }
 
 const style: Stylesheet = {
   contentContainer: {
-    minHeight: '100%',
+    [mq[768]]: {
+      alignItems: 'flex-start',
+    },
   },
   header: {
     ...Font.h1Green,
     margin: '0 0 1.2em 0',
+    textAlign: 'center',
+    [mq[768]]: {
+      textAlign: 'left',
+    },
+  },
+  detailsContainer: {
+    [mq[768]]: {
+      paddingRight: '1em',
+    },
   },
 }
