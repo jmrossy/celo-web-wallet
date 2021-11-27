304cc5a603cb187c1b87a33fdda3bca4ce53c299
J M Rossy <jm.rossy@gmail.com>
2021-11-27 11:41:37 +0100

    Remove unused test libs

--- spec/helpers/global.js
+++ spec/helpers/global.js
@@ -2,3 +2,18 @@ global.__DEBUG__ = true
 global.__IS_ELECTRON__ = false
 global.__VERSION__ = '1.0.0'
 global.fetch = require('node-fetch')
+
+var localStorage = {}
+localStorage.setItem = function (key, val) {
+  this[key] = val + ''
+}
+localStorage.getItem = function (key) {
+  return this[key]
+}
+Object.defineProperty(localStorage, 'length', {
+  get: function () {
+    return Object.keys(this).length - 2
+  },
+})
+global.localStorage = localStorage
+global.navigator = {}
